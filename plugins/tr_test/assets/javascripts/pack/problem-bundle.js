9ca31e7950140cafcbb659f42360094a1201a9eb
Your Name <tr990511@163.com>
2020-02-19 09:15:45 +0800

    fix

--- plugins/tr_test/assets/javascripts/pack/problem-bundle.js
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import Problem from '../bundles/Problem/components/Problem';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  Problem,
-});
