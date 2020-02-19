3c2918ad1acd1b952b2824be89af6e5e0e7b60e0
tr990511 <tr990511@163.com>
2020-02-19 09:59:49 +0800

    rout

--- plugins/tr_test/config/routes.rb
+++ plugins/tr_test/config/routes.rb
@@ -1,7 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get 'problems', :to => 'problem#index'
-post 'problem/:id', :to => 'problem#add'
+post 'problem', :to => 'problem#add'
 get 'problem/:id', :to => 'problem#show'
 put 'problem/:id', :to => 'problem#edit'
 delete 'problem/:id', :to => 'problem#delete'
