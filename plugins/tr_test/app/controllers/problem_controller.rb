e21b6c427f5357fee3fb76937cf13707b4375d3b
tr990511 <tr990511@163.com>
2020-02-19 09:08:41 +0800

    add debugger

--- plugins/tr_test/app/controllers/problem_controller.rb
+++ plugins/tr_test/app/controllers/problem_controller.rb
@@ -1,20 +1,26 @@
 class ProblemController < ApplicationController
 
   def index
+    debugger
   end
 
   def show
+    debugger
   end
 
   def add
+    debugger
   end
 
   def edit
+    debugger
   end
 
   def show
+    debugger
   end
 
   def delete
+    debugger
   end
 end
