d78647559cd098a11a6c9838f369282f63503f3c
Your Name <tr990511@163.com>
2020-02-19 09:20:51 +0800

    control

--- plugins/tr_test/app/controllers/problem_controller.rb
+++ plugins/tr_test/app/controllers/problem_controller.rb
@@ -1,20 +1,27 @@
 class ProblemController < ApplicationController
 
   def index
+  #123
+    debugger
   end
 
   def show
+    debugger
   end
 
   def add
+    debugger
   end
 
   def edit
+    debugger
   end
 
   def show
+    debugger
   end
 
   def delete
+    debugger
   end
 end
