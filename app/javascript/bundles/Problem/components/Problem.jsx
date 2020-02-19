0e1bb22d0a3e49ce2c80b47f5039ba6e95c07b89
tr990511 <tr990511@163.com>
2020-02-19 15:43:13 +0800

    fix problem

--- app/javascript/bundles/Problem/components/Problem.jsx
+++ app/javascript/bundles/Problem/components/Problem.jsx
@@ -30,7 +30,7 @@ export default class Problem extends React.Component {
                cache: 'default',
 			   body };
 
-		fetch("/problems")//,myInit
+		fetch("/problem/1")//,myInit
 		.then(res => res.text())
 		.then(
 			data => {
