1c21860350bb967f08196269e5cff8774d14cb13
tr990511 <tr990511@163.com>
2020-02-19 13:42:58 +0800

    add react

---
+++ app/javascript/packs/problem-bundle.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import Problem from '../bundles/Problem/components/Problem';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  Problem,
+});
