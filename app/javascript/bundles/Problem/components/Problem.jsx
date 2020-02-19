0c20873a04cf76ba0128a94a6336f5b9fcca7026
tr990511 <tr990511@163.com>
2020-02-19 13:50:57 +0800

    fix problem

--- app/javascript/bundles/Problem/components/Problem.jsx
+++ app/javascript/bundles/Problem/components/Problem.jsx
@@ -1,45 +1,37 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class HelloWorld extends React.Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired, // this is passed from the Rails view
-  };
-
-  /**
-   * @param props - Comes from your rails view.
-   */
-  constructor(props) {
-    super(props);
-
-    // How to set initial state in ES6 class syntax
-    // https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
-    this.state = { name: this.props.name };
-  }
-
-  updateName = (name) => {
-    this.setState({ name });
-  };
-
-  render() {
-    return (
-      <div>
-        <h3>
-          Hello, tr-{this.state.name}!
-        </h3>
-        <hr />
-        <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
-        </form>
-      </div>
-    );
-  }
+export default class Problem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {value: 'mobx'};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	// 监听下拉列表的变化
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+	// 表单提交的响应函数
+	handleSubmit(event) {
+		alert('You picked ' + this.state.value);
+		event.preventDefault();
+	}
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+				Pick one library:
+				{/* select的value属性定义当前哪个option元素处于选中状态
+				*/}
+				<select value={this.state.value} onChange={this.handleChange}>
+					<option value="react">React</option>
+					<option value="redux">Redux</option>
+					<option value="mobx">MobX</option>
+				</select>
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+		);
+	}
 }
